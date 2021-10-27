64ec3b426c58dae2d33b995dc49de7eee206fdde
Arun Babu PT <36483987+ptarun@users.noreply.github.com>
2021-10-28 00:33:19 +0530

    Delete IVote/.history directory

--- IVote/.history/generateResult_20211026092501.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-class generateResult:
-
-    def genResult(election_id):
-        conn = sqlite3.connect("ivote.db")
-        c = conn.cursor()
--- IVote/.history/test_20211026182254.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-
-conn = sqlite3.connect("ivote.db")
-c = conn.cursor()
-c.execute("insert into admin values(admin@gmail.com,admin)")
